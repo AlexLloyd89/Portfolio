d9d86887fc1be3025815b1a0c8c54d2bf791a30f
AlexLloyd89 <42323618+AlexLloyd89@users.noreply.github.com>
2018-11-24 20:30:52 -0500

    Add files via upload

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,7 @@
-$('#menuToggle input').on('click', function(){
-$('.contain:visible').fadeOut('fast');
-$('.contain:hidden').fadeIn('slow');
+$(document).ready(function() {
+  //Toggle hero image text display on menu click
+  $("#menuToggle input").on("click", function() {
+    $(".contain:visible").fadeOut("fast");
+    $(".contain:hidden").fadeIn("slow");
+  });
 });
-
-
-
-
